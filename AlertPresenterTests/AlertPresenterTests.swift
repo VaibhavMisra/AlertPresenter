fd5a8e5741200ac92979e77eddae2ad31ccb20c2
VaibhavMisra <vaibhavmisra.1987@gmail.com>
2018-09-28 10:33:55 +0530

    Added unit test for alert

--- AlertPresenterTests/AlertPresenterTests.swift
+++ AlertPresenterTests/AlertPresenterTests.swift
@@ -10,25 +10,38 @@ import XCTest
 @testable import AlertPresenter
 
 class AlertPresenterTests: XCTestCase {
-
+    
+    var vc: ViewController!
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let mainSB = UIStoryboard(name: "Main", bundle: nil)
+        self.vc = (mainSB.instantiateViewController(withIdentifier: "homeVC") as! ViewController)
+        UIApplication.shared.keyWindow?.rootViewController = self.vc
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testAlertHasTitle() {
+        vc.showAlert(UIButton())
+        
+        XCTAssertTrue(vc.presentedViewController is UIAlertController)
+        XCTAssertEqual(vc.presentedViewController?.title, "Test Alert")
     }
-
+    
+    func testAlertFirstActionTitleIsOK() {
+        
+        vc.showAlert(UIButton())
+        let alertController = vc.presentedViewController as! UIAlertController
+        let action = alertController.actions.first!
+        XCTAssertEqual(action.title, "Ok")
+    }
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
